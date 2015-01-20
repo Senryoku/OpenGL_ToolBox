cb85d3a83dbda8e812e7d2ea6b5ef07ee6d9ddce
Senryoku <maretverdant@gmail.com>
2015-01-20 16:33:55 +0100

    Textured Ground Water

--- src/GLSL/Water/draw_fs.glsl
+++ src/GLSL/Water/draw_fs.glsl
@@ -1,5 +1,7 @@
 #version 430
 
+uniform sampler2D GroundTexture;
+
 in layout(location = 0) vec4 in_position;
 in layout(location = 1) vec3 normal;
 in layout(location = 2) vec2 texcoord;
@@ -15,7 +17,7 @@ void main()
 {
 	vec3 color = vec3(0.5, 0.5, 1.0);
 	if(ground > 0.0)
-		color = vec3(0.2);
+		color = texture(GroundTexture, texcoord).xyz;
 	colorMaterialOut = vec4(color, 0.0);
 	worldPositionOut = vec4(in_position.xyz, gl_FragCoord.z);
 	worldNormalOut = vec4(normal, 1.0);
